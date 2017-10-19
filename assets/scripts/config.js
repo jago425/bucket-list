53498a0bb886b9211cbee040567cbe973befcfb5
jago425 <rmjagodowski@gmail.com>
2017-10-19 19:06:22 -0400

    added api config and authentication
    -api.js added ajax calls for authentication
    -events.js added click handlers for authentication
    -ui.js added ui for authentication

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,5 +1,5 @@
 'use strict'
-
+// this is telling the front-end where to find the API...not where the front end should be running
 const config = {
   apiOrigins: {
     production: 'https://ga-wdi-boston.herokuapp.com',
