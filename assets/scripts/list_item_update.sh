bbc1123eab37bd19e73c1ea7350b08073b1786c3
jago425 <rmjagodowski@gmail.com>
2017-10-22 23:49:11 -0400

    added curl requests

---
+++ assets/scripts/list_item_update.sh
@@ -0,0 +1,22 @@
+curl --include --request PATCH "http://localhost:4741/list_items/${ID}" \
+--header "Authorization: Token token=$TOKEN" \
+--header "Content_Type: application/json" \
+--data '{
+  "list_item": {
+    "done": "'"${DONE}"'"
+  }
+}'
+
+
+echo
+
+curl --include --request PATCH "http://localhost:4741/list_items/${ID}" \
+--header "Authorization: Token token=$TOKEN" \
+--header "Content_Type: application/json" \
+--data '{
+  "list_item": {
+    "item_description": "'"${ITEM_DESCRIPTION}"'"
+  }
+}'
+
+echo
