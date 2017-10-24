382b3d07f8c47fd932bed10f3a8731a5ce10b7bb
jago425 <rmjagodowski@gmail.com>
2017-10-23 23:17:50 -0400

    updated production URL in config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 // this is telling the front-end where to find the API...not where the front end should be running
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com',
+    production: 'https://fathomless-lake-16939.herokuapp.com',
     development: 'http://localhost:4741'
   }
 }
